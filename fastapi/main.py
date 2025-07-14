bb910c890ee76f5e043219892ba83555bca78188
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2025-07-14 20:27:33 +0200

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -352,4 +352,4 @@ def get_top_producing_areas(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
